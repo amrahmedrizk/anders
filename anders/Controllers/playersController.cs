e5780c8188a48f2cf53988c1ccfcc7a6d4c8563d
ST1 <ST1@DESKTOP-454OO7G>
2020-02-02 16:47:49 +0200

    install

---
+++ anders/Controllers/playersController.cs
@@ -0,0 +1,72 @@
+﻿using anders.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace anders.Controllers
+{
+    public class playersController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: players/Create
+        public ActionResult Create()
+        {
+            
+            if (Session["uu"] != null)
+            {
+               var p = (List<players>)Session["uu"];
+                return View("Create", p);
+
+            }
+            else
+            {
+                List<players> p = new List<players>();
+
+                p = db.players.ToList();
+
+                p.Sort();
+                return View("Create", p);
+
+            }
+
+        }
+
+        // POST: players/Create
+        [HttpPost]
+        public ActionResult Create(players player)
+        {
+            if (player.name != null && player.score != 0)
+            {
+                //var fg = db.players.ToList();
+                //db.players.RemoveRange(fg);
+                //db.SaveChanges();
+                string n = player.name;
+                int ss = player.score;
+                players oo = new players { name = n, score = ss };
+                db.players.Add(oo);
+                db.SaveChanges();
+            }
+           
+
+          var p = db.players.ToList();
+
+            p.Sort();
+
+            
+            Session["uu"] = p;
+           
+            return View("Create", p);
+
+
+        }
+
+
+
+
+    }
+}
---
+++ anders/Models/players.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace anders.Models
+{
+    public class players : IComparable<players>
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int score { get; set; }
+
+        public int CompareTo(players x)
+        {
+            //return this.score.CompareTo(x.score);
+            if (this.score < x.score)
+            {
+                return 1;
+
+            }
+            else if (this.score > x.score)
+            {
+                return -1;
+            }
+            else
+                return 0;
+        }
+    }
+}
